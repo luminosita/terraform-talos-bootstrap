e4206fb6e016b8bd8ef751e80bf43fdd9741c91e
Milos Milosavljevic <milosh@emisia.net>
2025-04-22 00:15:50 +0200

    health commented

--- config.tf
+++ config.tf
@@ -93,20 +93,21 @@ resource "talos_machine_bootstrap" "this" {
   client_configuration = talos_machine_secrets.this.client_configuration
 }
 
-data "talos_cluster_health" "this" {
-  depends_on = [
-    talos_machine_configuration_apply.this,
-    talos_machine_bootstrap.this
-  ]
-  skip_kubernetes_checks = false
-  client_configuration   = data.talos_client_configuration.this.client_configuration
-  control_plane_nodes    = [for k, v in var.nodes : v.network.ip if v.machine_type == "controlplane"]
-  worker_nodes           = [for k, v in var.nodes : v.network.ip if v.machine_type == "worker"]
-  endpoints              = data.talos_client_configuration.this.endpoints
-  timeouts = {
-    read = "15m"
-  }
-}
+#FIXME: remove comment
+# data "talos_cluster_health" "this" {
+#   depends_on = [
+#     talos_machine_configuration_apply.this,
+#     talos_machine_bootstrap.this
+#   ]
+#   skip_kubernetes_checks = false
+#   client_configuration   = data.talos_client_configuration.this.client_configuration
+#   control_plane_nodes    = [for k, v in var.nodes : v.network.ip if v.machine_type == "controlplane"]
+#   worker_nodes           = [for k, v in var.nodes : v.network.ip if v.machine_type == "worker"]
+#   endpoints              = data.talos_client_configuration.this.endpoints
+#   timeouts = {
+#     read = "15m"
+#   }
+# }
 
 resource "talos_cluster_kubeconfig" "this" {
   depends_on = [talos_machine_bootstrap.this]
